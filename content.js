bbd8adac1391b04ea226963081bbfdba8530ced7
skkiii-tomato <spousti2031@francisparker.org>
2025-11-06 11:02:13 -0800

    Create content.js

---
+++ content.js
@@ -0,0 +1,6 @@
+title = document.querySelector("title");
+title.innerHTML = "Britannica";
+
+link = document.querySelector("link[rel~='icon']");
+
+link.href = "https://www.shareicon.net/data/512x512/2015/09/25/107042_new_512x512.png";
